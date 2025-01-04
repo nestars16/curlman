c40dfca5ca77b2c0ae010155a7bf5bccbc3d4252
nestars16 <nesmb16@gmail.com>
2025-01-04 12:34:58 -0500

    Starting to add utf8 safe cursor movements

--- src/cursor_movements.rs
+++ src/cursor_movements.rs
@@ -93,20 +93,49 @@ impl CursorMoveDirection {
     ) -> Option<(usize, usize)> {
         use CursorMoveDirection::*;
 
+        fn get_the_beginning_byte_of_last_character(line: &str) -> usize {
+            let mut beginning_byte_of_last_character = line.len();
+            while !line.is_char_boundary(beginning_byte_of_last_character) {
+                beginning_byte_of_last_character -= 1
+            }
+            beginning_byte_of_last_character
+        }
+
         fn fit_col(col: usize, line: &str) -> usize {
-            cmp::min(col, line.chars().count())
+            if col >= line.len() {
+                get_the_beginning_byte_of_last_character(line)
+            } else if !line.is_char_boundary(col) {
+                let mut beginning_of_utf8_char_near_col = col - 1;
+                while !line.is_char_boundary(beginning_of_utf8_char_near_col) {
+                    beginning_of_utf8_char_near_col -= 1;
+                }
+                beginning_of_utf8_char_near_col
+            } else {
+                col
+            }
         }
 
         match self {
-            Forward if col >= lines[row].chars().count() => {
-                (row + 1 < lines.len()).then(|| (row + 1, 0))
+            Forward if col >= lines[row].len() => (row + 1 < lines.len()).then(|| (row + 1, 0)),
+            Forward => {
+                let mut bytes_forward = 1;
+                while !lines[row].is_char_boundary(col + bytes_forward) {
+                    bytes_forward += 1
+                }
+                Some((row, col + bytes_forward))
             }
-            Forward => Some((row, col + 1)),
             Back if col == 0 => {
                 let row = row.checked_sub(1)?;
-                Some((row, lines[row].chars().count()))
+                Some((row, get_the_beginning_byte_of_last_character(&lines[row])))
             }
-            Back => Some((row, col - 1)),
+            Back => {
+                let current_line = lines[row].as_str();
+                let mut bytes_backward = 1;
+                while !current_line.is_char_boundary(col - bytes_backward) {
+                    bytes_backward -= 1
+                }
+                Some((row, col.checked_sub(bytes_backward)?))
+            }
             Up => {
                 let row = row.checked_sub(1)?;
                 Some((row, fit_col(col, &lines[row])))
@@ -119,46 +148,10 @@ impl CursorMoveDirection {
                 let row = lines.len() - 1;
                 Some((row, fit_col(col, &lines[row])))
             }
-            WordEnd => {
-                // `+ 1` for not accepting the current cursor position
-                if let Some(col) = find_word_inclusive_end_forward(&lines[row], col + 1) {
-                    Some((row, col))
-                } else {
-                    let mut row = row;
-                    loop {
-                        if row == lines.len() - 1 {
-                            break Some((row, lines[row].chars().count()));
-                        }
-                        row += 1;
-                        if let Some(col) = find_word_inclusive_end_forward(&lines[row], 0) {
-                            break Some((row, col));
-                        }
-                    }
-                }
-            }
-            WordForward => {
-                if let Some(col) = find_word_start_forward(&lines[row], col) {
-                    Some((row, col))
-                } else if row + 1 < lines.len() {
-                    Some((row + 1, 0))
-                } else {
-                    Some((row, lines[row].chars().count()))
-                }
-            }
-            WordBack => {
-                if let Some(col) = find_word_start_backward(&lines[row], col) {
-                    Some((row, col))
-                } else if row > 0 {
-                    Some((row - 1, lines[row - 1].chars().count()))
-                } else {
-                    Some((row, 0))
-                }
-            }
-            Jump(row, col) => {
-                let row = cmp::min(*row as usize, lines.len() - 1);
-                let col = fit_col(*col as usize, &lines[row]);
-                Some((row, col))
-            }
+            WordForward => None,
+            WordEnd => None,
+            WordBack => None,
+            Jump(_, _) => None,
         }
     }
 }
